93d0ffd217303361b3bf40c2a5cc5794e428a19f
Pranav Kalra <42677927+pranav-kalra@users.noreply.github.com>
2019-02-28 11:55:42 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fighter PlayerA = new Fighter("MoAli");
+        PlayerA.showStats();
+
+        Mage PlayerB = new Mage("HarryPotter");
+        PlayerB.showStats();
+
+        Priest PlayerC = new Priest("JSerra");
+        PlayerC.showStats();
+
+        Paladin PlayerD = new Paladin("Knighttime");
+        PlayerD.showStats();
+
+//        PlayerCharacter playerX = new PlayerCharacter();
+
+
+
+
+
+    }
+}
---
+++ PlayerCharacter.java
@@ -0,0 +1,18 @@
+public abstract class PlayerCharacter {
+
+
+    String name;
+
+    int strength;
+    int intelligence;
+    int agility;
+
+    int hp, hpmax;
+    int mp, mpmax;
+
+    public PlayerCharacter() {
+        System.out.println("Player Character extended class created....");
+
+
+    }
+}
